bfab6c5548b7fbc5673ed66fcf78ddc1517e8060
George Argyros <argyros.george@gmail.com>
2016-07-11 13:10:56 -0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'sflearn',
+  packages = ['sflearn'],
+  version = '0.1',
+  description = 'Transducer inference library',
+  author = 'George Argyros',
+  author_email = 'argyros.george@gmail.com',
+  url = 'https://github.com/GeorgeArgyros/sflearn',
+  download_url = 'https://github.com/GeorgeArgyros/sflearn/tarball/0.1',
+  keywords = ['Transducers', 'Automata', 'Learning', 'Sanitizers'],
+  classifiers = [],
+)
+
